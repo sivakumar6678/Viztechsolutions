83afcdd1e645fa4f0044f12e83bf3a06ed7bb5b0
sivakumar6678 <chandragarisivakumar@gmail.com>
2025-06-13 18:48:03 +0530

    Add ProductDesigning component with styles and animations for enhanced user experience

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import './Services.css';
 import ServicesList from '../components/services/ServicesList';
+import ProductDesigning from '../components/services/ProductDesigning';
 import ServicesProcess from '../components/services/ServicesProcess';
 import ServicesTestimonials from '../components/services/ServicesTestimonials';
 import ServicesCTA from '../components/services/ServicesCTA';
@@ -14,6 +15,7 @@ const Services = () => {
   return (
     <div className="services-page">
       <ServicesList />
+      <ProductDesigning />
       <ServicesProcess />
       <ServicesTestimonials />
       <ServicesCTA />
